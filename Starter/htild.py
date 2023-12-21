27f2700dd814adbcc54272655eb5798714aaafbf
Carole Perigois <caroleperigois@outlook.com>
2023-12-21 16:50:25 +0100

    pep 8 modules names

---
+++ Starter/htild.py
@@ -0,0 +1,299 @@
+import os
+import pycbc.waveform
+from astropy.cosmology import Planck15
+import Stochastic.constants as K
+import numpy as np
+import Stochastic.basic_functions as BF
+import math
+from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.special import jv
+from scipy.integrate import quad
+from scipy.optimize import fsolve
+import Stochastic.pix
+import warnings
+
+
+
+
+def GWk(evt:np.ndarray , type:str, inc:float = None) :
+    """
+    NOT ADAPTED YET TO THE DATAFRAME FORMAT.
+    Compute the contribution of a binary to the background with analytical waveforms from 0909.2867, and 1210.6666.
+    Parameters
+    ----------
+    :param evt (1Darray):
+    :param type:
+    :param inc:
+    :return:
+    """
+
+    Mc = evt[0]
+    q = evt[1]
+    Spin = evt[2]
+    z = evt[3]
+    zf = evt[4]
+    a0 = evt[5]
+    ecc = evt[6]
+    if inc == 0 :
+        Fi = 4./5.
+    else :
+        Fi = pow(1. + pow(math.cos(float(inc)), 2.), 2.) / 4. + pow(math.cos(float(inc)), 2.)
+
+    f = GS.Freq
+    mtot = BF.mass1(Mc, q) + BF.mass2(Mc, q)
+    eta = BF.mass1(Mc, q) * BF.mass2(Mc, q) / (math.pow(mtot, 2.))
+    Omg = np.zeros(len(f))
+    Omg_e0 = np.zeros(len(f))
+
+    if float(ecc) == 1.:  # check that the eccentricity is not 1
+        e0 = 0.9999
+    else:
+        e0 = float(ecc)
+    if float(a0) == 0 :
+        f0 = 1
+    else :
+        f0 = 1.e-4 * math.pow(float(a0) + 1, -3. / 2.) * math.pow(mtot, 0.5)
+    if f0 < 100000:  # old test now f0 always >1000
+        f_lso = 4394.787 / (mtot * (1 + z))
+        f_merg = BF.fmerg_f(BF.mass1(Mc, q), BF.mass2(Mc, q), Spin, z)
+        f_ring = BF.fring_f(BF.mass1(Mc, q), BF.mass2(Mc, q), Spin, z)
+        f_cut = BF.fcut_f(BF.mass1(Mc, q), BF.mass2(Mc, q), Spin, z)
+        sigma = BF.sigma_f(BF.mass1(Mc, q), BF.mass2(Mc, q), Spin, z)
+        if ((type == 'BNS') or (type == 'BHNS')):
+            f_merg = f_lso
+            f_ring = 9.e-7
+            f_cut = 9.e-7
+
+        # Parameters calculations
+        a2 = -323. / 224. + 451. * eta / 168.
+        a3 = (27. / 8. - 11. * eta / 6.) * Spin
+        e1 = 1.4547 * Spin - 1.8897
+        e2 = -1.8153 * Spin + 1.6557
+        nu_merg = math.pow(mtot * (1 + z) * (f_merg) * 1.547388618e-5, 1. / 3.)
+        nu_ring = math.pow(mtot * (1 + z) * (f_ring) * 1.547388618e-5, 1. / 3.)
+        g_merg = math.pow(1. + a2 * nu_merg * nu_merg + a3 * nu_merg * nu_merg * nu_merg, 2.)
+        g_ring1 = math.pow(1. + e1 * nu_merg + e2 * nu_merg * nu_merg, 2.)
+        g_ring2 = math.pow(1. + e1 * nu_ring + e2 * nu_ring * nu_ring, 2.)
+        wm = g_merg / (g_ring1 * f_merg)
+        wr = wm * g_ring2 * math.pow(f_ring, -4. / 3.)
+        # Doing the interpolation between the observed frequency and the redshift
+        if abs(float(zf) - z) >= 0.05:
+            z_int = []
+            f_int = []
+            z2 = float(zf)
+            z = z2
+            f_pre = math.pow(f0, -8. / 3.)
+            #k = 145754753.28 * math.pow(Mc, 5. / 3.)
+            while z > float(z):  # graphe temps red freq, verifier sans ecc et sans Q
+                tau_z = quad(BF.tau_Myr, z, z2)
+                f1 = pow(f_pre, -8. / 3.) - K.Cst * tau_z[0] * pow(Mc * K.M_sun, -5. / 3.) * K.yr * 1.e6
+                f_int.append(math.pow(f1, -3. / 8.) / (1. + z))
+                z_int.append(z)
+                f_pre = pow(f1, -3. / 8.)
+                z2 = z
+                z = z - 0.001
+            int_zf = InterpolatedUnivariateSpline(f_int, z_int)
+        z = float(zf)
+        en = [e0, e0, e0, e0, e0, e0, e0, e0]
+        i = 0
+        if f_cut == 9.e-7:  # if the source is a BNS or a NSBH:
+            f_cut = f_lso  # turn on frequencies until the last stable orbit
+            wr = 0  # No ringdown
+        while (f[i] < f_cut):
+            n = 2
+            while n < 6:
+                if (z - z) >= 0.05:
+                    z = abs(int_zf(f[i] / n))
+                else:
+                    z = z
+                if f[i] > n * f0 / (1 + z):
+                    if type == 'BBH':
+                        nu = math.pow(mtot * (1 + z) * f[i] * 1.547388618e-5, 1. / 3.)
+                        g_merg = math.pow(1. + a2 * nu * nu + a3 * nu * nu * nu, 2.)
+                        g_ring = math.pow(1. + e1 * nu + e2 * nu * nu, 2.)
+                    else:
+                        g_merg = 1.
+                    Dl = Planck15.luminosity_distance(z).value * K.Mpc# check units !!!
+                    K2 = pow(Mc * K.M_sun * (1 + z), 5. / 3.) / (Dl * Dl )
+                    if n == 2:  # Data for circular orbits
+                        if f[i] < f_merg:
+                            Omg_e0[i] += g_merg * K.K1 * K2 * math.pow(f[i], 2. / 3.) * Fi
+                        elif f[i] < f_ring:
+                            Omg_e0[i] += wm * g_ring * K.K1 * K2 * math.pow(f[i], 5. / 3.) * Fi
+                        else:
+                            Omg_e0[i] += wr * K.K1 * K2 * f[i] * math.pow((f[i]) / (
+                                        1 + (f[i] - f_ring) / (sigma / 2.) * (f[i] - f_ring) / (
+                                        sigma / 2.)), 2.) * Fi
+
+                    if en[n] > 0.00105:  # If the eccentricity is higher than 0.00105 we take the harmonics into account
+                        e = fsolve(BF.fe3, 1.e-7, args=(mtot, float(a0), e0, f[i] * (1 + z) / (float(n))))
+                        psi_e = (1 + 73. * e[0] * e[0] / 74. + 37. * e[0] * e[0] * e[0] * e[0] / 96.) / (
+                            pow(1 - e[0] * e[0], 7. / 2.))
+                        g_ne = math.pow(4. / float(n * n), 1. / 3.) * pow(n, 4.) / 32 * (
+                                pow(jv(n - 2, n * e[0]) - 2 * e[0] * jv(n - 1, n * e[0]) + 2 / n * jv(n,
+                                                                                                      n * e[
+                                                                                                          0]) + 2 *
+                                    e[0] * jv(n + 1, n * e[0]) - jv(n + 2, n * e[0]), 2.) + (
+                                        1 - e[0] * e[0]) * pow(
+                            jv(n - 2, n * e[0]) - 2 * jv(n, n * e[0]) + jv(n + 2, n * e[0]), 2.) + 4 / (
+                                        3 * n * n) * jv(n, n * e[0]) * jv(n, n * e[0]))
+                        en[n] = e[0]
+                        n += 1
+                    else:  # If the eccentricity is low we do not calculate harmonics above n = 2
+                        psi_e = 1.
+                        g_ne = math.pow(4. / float(n * n), 1. / 3.)
+                        en[n] = 0
+                        n += 6
+                    if f[i] < f_merg:
+                        Omg[i] += g_merg * K.K1 * K2 * math.pow(f[i], 2. / 3.) * g_ne / psi_e * Fi
+                    elif f[i] < f_ring:
+                        Omg[i] += wm * g_ring * K.K1 * K2 * math.pow(f[i], 5. / 3.) * g_ne / psi_e * Fi
+                    else:
+                        Omg[i] += wr * K.K1 * K2 * f[i] * math.pow(
+                                (f[i]) / (1 + (f[i] - f_ring) / (sigma / 2.) * (f[i] - f_ring) / (sigma / 2.)),
+                                2.) * g_ne / psi_e * Fi
+                else:
+                    n = n + 1
+            i += 1
+    return Omg, Omg_e0
+
+def GWk_noEcc(evt, type, inc = None) :
+
+    """
+        Parameters
+        ----------
+        args : numpy array
+            Given by the re-built catalogue
+        f : numpy array
+            Observed frequency range for the calculation. Has to be the same for all sources from a catalogue
+
+        Returns
+        -------
+        Omg : numpy array
+            Size of f with the contribution of the source for each observed frequency
+        """
+
+    Mc = evt[0]
+    q = evt[1]
+    Spin = evt[2]
+    z = evt[3]
+    zf = z
+    if inc == 0 :
+        Fi = 4./5.
+    else :
+        Fi = pow(1. + pow(math.cos(float(inc)), 2.), 2.) / 4. + pow(math.cos(float(inc)), 2.)
+
+    f = GS.Freq
+    m1 = BF.mass1(Mc, q)
+    m2 = BF.mass2(Mc, q)
+    mtot = m1 + m2
+    eta = m1 * m2 / (math.pow(mtot, 2.))
+    Omg_e0 = np.zeros(len(f))
+
+    f_lso = 4394.787 / (mtot * (1 + z))
+    f_merg = BF.fmerg_f(m1,m2 , Spin, z)
+    f_ring = BF.fring_f(m1, m2, Spin, z)
+    f_cut = BF.fcut_f(BF.mass1(Mc, q), BF.mass2(Mc, q), Spin, z)
+    sigma = BF.sigma_f(BF.mass1(Mc, q), BF.mass2(Mc, q), Spin, z)
+    if f_merg>1. :
+        if ((type == 'BNS') or (type == 'BHNS')):
+            f_merg = f_lso
+            f_ring = 9.e-7
+            f_cut = 9.e-7
+        # Parameters calculations
+        a2 = -323. / 224. + 451. * eta / 168.
+        a3 = (27. / 8. - 11. * eta / 6.) * Spin
+        e1 = 1.4547 * Spin - 1.8897
+        e2 = -1.8153 * Spin + 1.6557
+        nu_merg = math.pow(mtot * (1 + z) * (f_merg) * 1.547388618e-5, 1. / 3.)
+        nu_ring = math.pow(mtot * (1 + z) * (f_ring) * 1.547388618e-5, 1. / 3.)
+        g_merg = math.pow(1. + a2 * nu_merg * nu_merg + a3 * nu_merg * nu_merg * nu_merg, 2.)
+        g_ring1 = math.pow(1. + e1 * nu_merg + e2 * nu_merg * nu_merg, 2.)
+        g_ring2 = math.pow(1. + e1 * nu_ring + e2 * nu_ring * nu_ring, 2.)
+        wm = g_merg / (g_ring1 * f_merg)
+        wr = wm * g_ring2 * math.pow(f_ring, -4. / 3.)
+        z = float(zf)
+        i = 0
+        if f_cut == 9.e-7:  # if the source is a BNS or a NSBH:
+            f_cut = f_lso  # turn on frequencies until the last stable orbit
+            wr = 0  # No ringdown
+        while (f[i] < f_cut):
+            n = 2
+            if type == 'BBH':
+                nu = math.pow(mtot * (1 + z) * f[i] * 1.547388618e-5, 1. / 3.)
+                g_merg = math.pow(1. + a2 * nu * nu + a3 * nu * nu * nu, 2.)
+                g_ring = math.pow(1. + e1 * nu + e2 * nu * nu, 2.)
+            else:
+                g_merg = 1.
+            Dl = Planck15.luminosity_distance(z).value * K.Mpc# check units !!!
+            K2 = pow(Mc * K.M_sun * (1 + z), 5. / 3.) / (Dl * Dl )
+            if f[i] < f_merg:
+                Omg_e0[i] += g_merg * K.K1 * K2 * math.pow(f[i], 2. / 3.) * Fi
+            elif f[i] < f_ring:
+                Omg_e0[i] += wm * g_ring * K.K1 * K2 * math.pow(f[i], 5. / 3.) * Fi
+            else:
+                Omg_e0[i] += wr * K.K1 * K2 * f[i] * math.pow((f[i]) / (
+                                            1 + (f[i] - f_ring) / (sigma / 2.) * (f[i] - f_ring) / (
+                                            sigma / 2.)), 2.) * Fi
+            i+=1
+    return Omg_e0
+
+
+def GWk_noEcc_Pycbcwf(evt, freq, approx, n, size_catalogue, inc_option = 'InCat') :
+    """This function calculate the contribution of a binary
+        Parameters
+        ----------
+        args : numpy array
+            Given by the re-built catalogue
+        f : numpy array
+            Observed frequency range for the calculation. Has to be the same for all sources from a catalogue
+
+        Returns
+        -------
+        Omg : numpy array
+            Size of f with the contribution of the source for each observed frequency
+        """
+    warnings.filterwarnings("ignore")
+    flow = int(np.min(freq))
+    deltaf = int(freq[1]-freq[0])
+    if inc_option == 'InCat':
+        inc = evt.inc
+    elif inc_option == 'Rand' :
+        inc = np.arccos(np.random.uniform(-1, 1))
+    elif inc_option == 'Optimal' :
+        inc = 0.
+    flim= BF.fcut_f(m1 = evt.m1, m2 = evt.m2, xsi = 0, zm = evt.z).values
+    if flim>flow:
+        hptild, hctild = pycbc.waveform.get_fd_waveform(approximant = approx,
+                                                        mass1 = evt.m1 * (1. + evt.z),
+                                                        mass2 = evt.m2 * (1. + evt.z),
+                                                        spin1x = 0., spin1y=0., spin1z=evt.s1,
+                                                        spin2x = 0., spin2y=0., spin2z=evt.s2,
+                                                        delta_f=deltaf,
+                                                        f_lower=flow,
+                                                        inclination= inc,
+                                                        distance=evt.Dl, f_ref=20.)
+        #hptild = hptild[flow:]
+        #hctild = hctild[flow:]
+        if len(hptild) < len(freq):
+            hptild = np.concatenate((hptild, np.zeros(len(freq) - len(hptild))))
+            hctild = np.concatenate((hctild, np.zeros(len(freq) - len(hctild))))
+        elif len(hptild) > len(freq):
+            hptild = hptild[:len(freq)]
+            hctild = hctild[:len(freq)]
+        htildSQ = np.array(hptild * np.conjugate(hptild) + hctild * np.conjugate(hctild), dtype=float)
+        htildSQ = np.nan_to_num(htildSQ, nan=0.0)
+    else :
+        htildSQ = 0
+        #print('Waveform calculation failed, parameters m1 = {0}, m2 = {1}, and z = {2} hence to a cut frequency below the start of the analysis fcut = {3} < flow = {4}.'.format(evt.m1.values, evt.m2.values, evt.z.values, BF.fcut_f(m1 = evt.m1, m2 = evt.m2, xsi = 0, zm = evt.z).values, flow))
+
+    Stochastic.Pix.bar_peach(n,size_catalogue)
+
+    return htildSQ
+
+
+
+
+
+
+
---
+++ Stochastic/princess.py
@@ -0,0 +1,183 @@
+import pandas as pd
+import os
+import Stochastic.snr as SNR
+import Stochastic.constants as K
+import numpy as np
+import Stochastic.basic_functions as BF
+from Starter.htild import GWk_noecc_pycbcwf
+
+
+class Princess:
+
+    def __init__(self, Freq, astromodel, approx, Networks, inclination, Omega_ana_freq = [10,25]):
+        """Create an instance of your calculations parameters.
+        Parameters
+        ----------
+        Freq : np.array
+            Frequency band of the caclulation
+        Omega_ana_freq : list of float
+            Frequency of reference for the built of analysis file
+        Network: np.array of Networks class type
+            For which Network is done the calculation
+
+        """
+        self.Freq = Freq
+        #self.dict_Networks = {  }
+        #for key in Neworks.keys() :
+        #    self.dict_Networks[key] = Detection.Network(name = key, compo = Networks[key][0] , pic_file = Networks[key][1], freq = Networks[key][2], efficiency = Networks[key][3], SNR_thrs =Networks[key][4] )
+
+        self.Networks = Networks
+        self.Omega_ana_freq = Omega_ana_freq
+        self.approx = approx
+        self.inclination = inclination
+        self.astromodel = astromodel
+
+
+    def Make_Ana_Output(self):
+        output_index = ['N_source'] + ['Omg_' + str(i) + '_Hz' for i in self.Omega_ana_freq] + ['SNR_Total'] + [
+            'SNR_Residual']
+        self.anadict = {}
+        for cat in self.astromodel.catalogs:
+            self.anadict[cat] = pd.DataFrame(index=output_index,
+                                             columns=['Total'] + [self.Networks[i].name for i in range(len(self.Networks))])
+        print(self.anadict)
+
+    def Write_results(self) :
+        check_file = os.path.exists('Results/Analysis/')
+        if check_file == False:
+            os.mkdir('Results/Analysis/')
+        for cat in self.astromodel.catalogs:
+            df = self.anadict[cat]
+            print('Results/Analysis/' + cat)
+            df.to_csv('Results/Analysis/' + cat, sep='\t')
+
+    def Omega_ecc(self, cat):
+
+        df = pd.read_csv('Catalogs/'+cat, delimiter = '\t', index_col = None )
+        check_file = os.path.exists('Results/Omega_e0/' + cat)
+        if check_file == False:
+            Omega = pd.DataFrame({'f':Freq, 'Total': np.zeros(len(self.Freq))})
+            Omega_e0 = pd.DataFrame({'f':Freq, 'Total': np.zeros(len(self.Freq))})
+            for n in self.Networks :
+                Omega[n]= np.zeros(len(self.Freq))
+                Omega_e0[n] = np.zeros(len(self.Freq))
+            for i in range(len(df['Mc'])) :
+                evt = df.iloc[i]
+                args = [ evt['Mc'], evt['q'], evt['Xsi'], evt['z'], evt['zf'], evt['a0'], evt['e0'] ]
+                Omg, Omg_e0 = GWk(args, co_type, 0 )
+                Omega['Total'] += Omg
+                Omega_e0['Total'] += Omg_e0
+                for N in self.Networks :
+                    if evt[N]< self.SNR_thrs[N] :
+                        Omega[N] += Omg
+                        Omega_e0[N] += Omg_e0
+            Omega.to_csv('Results/Omega/'+path, index = False, sep = '\t')
+            Omega_e0.to_csv('Results/Omega_e0/'+path, index = False, sep = '\t')
+        else :
+            Omega_e0 = pd.read_read_csvcsv('Results/Omega_e0/'+path, index_col = False, sep = '\t')
+        check_file = os.path.exists('Results/Analysis/' + path)
+        if check_file == False:
+            output_index = ['N_source'] + ['Omg_' + str(i) + '_Hz' for i in self.Omega_ana_freq] + ['SNR']
+            Ana = pd.DataFrame(index=output_index, columns=['Total'] + self.Networks)
+            Ana['Total']['N_source'] = 0
+            Ana['Total'][['Omg_'+str(i)+'_Hz' for i in self.Omega_ana_freq]] = Search_Omg(Omega_e0['Total'], self.Omega_ana_freq)
+            Ana['Total']['SNR'] = SNR.SNR_Omega(Omega_e0['Total'])
+            for N in range(len(self.Networks)):
+                Ana[Networks[N].name][['Omg_' + str(i) + '_Hz' for i in self.Omega_ana_freq]] = Search_Omg(Omega_e0[Networks[N].name], self.Omega_ana_freq)
+                Ana[N]['SNR'] = SNR.SNR_Omega(Omega_e0[Networks[N].name],N)
+                residual = df[df[N]<self.SNR_thrs[N]]
+                Ana[N]['Nsource'] = len(residual[N])
+                print(Ana[N])
+            Ana.to_csv('Results/Analysis/'+path, sep = '\t')
+
+    def Analysis(self, Networks) :
+        for c in range(len(self.astromodel.catalogs)):
+            cat = self.astromodel.catalogs[c]
+            Omega_e0 = pd.read_csv('Results/Omega_e0/' + cat, index_col=False, sep='\t')
+            Ana = self.anadict[cat]
+            for i in self.Omega_ana_freq :
+                Ana['Total']['Omg_' + str(i) + '_Hz' ] = BF.Search_Omg(Freq = Omega_e0['f'], Omega = Omega_e0['Total'], freq_ref = i)
+            for N in range(len(Networks)):
+                for i in self.Omega_ana_freq:
+                    Ana[Networks[N].name]['Omg_' + str(i) + '_Hz'] = BF.Search_Omg(Freq = Omega_e0['f'], Omega = Omega_e0[Networks[N].name], freq_ref = i)
+#                SNRres = SNR.SNR_bkg(Omega_e0['f'], Omega_e0[Networks[N].name], Networks[N])
+#                SNRtot = SNR.SNR_bkg(Omega_e0['f'],Omega_e0['Total'], Networks[N])
+#                print(SNRres,' ', Networks[N].name)
+                Ana[Networks[N].name]['SNR_Residual'] = SNR.SNR_bkg(Omega_e0['f'], Omega_e0[Networks[N].name], Networks[N])
+                Ana[Networks[N].name]['SNR_Total'] = SNR.SNR_bkg(Omega_e0['f'],Omega_e0['Total'], Networks[N])
+            self.anadict[cat] = Ana
+
+    def Analysis_noResidual(self, Networks):
+        for c in range(len(self.astromodel.catalogs)):
+            cat = self.astromodel.catalogs[c]
+            Omega_e0 = pd.read_csv('Results/Omega_e0/' + cat, index_col=False, sep='\t')
+            Ana = self.anadict[cat]
+            for i in self.Omega_ana_freq:
+                Ana['Total']['Omg_' + str(i) + '_Hz'] = BF.Search_Omg(Freq=Omega_e0['f'], Omega=Omega_e0['Total'],
+                                                                      freq_ref=i)
+            for N in range(len(Networks)):
+                for i in self.Omega_ana_freq:
+                    Ana[Networks[N].name]['Omg_' + str(i) + '_Hz'] = BF.Search_Omg(Freq=Omega_e0['f'],
+                                                                                   Omega=Omega_e0['Total'],
+                                                                                   freq_ref=i)
+                Ana[Networks[N].name]['SNR_Total'] = SNR.SNR_bkg(Omega_e0['f'], Omega_e0['Total'], Networks[N])
+            self.anadict[cat] = Ana
+
+    def Omega(self, cat, Freq, Networks):
+
+        df = pd.read_csv('Catalogs/'+cat, delimiter = '\t', index_col = None )
+        check_file = os.path.exists('Results/Omega_e0/' + cat)
+        if check_file == False:
+            Omega_e0 = pd.DataFrame({'f':Freq, 'Total': np.zeros(len(Freq))})
+            for n in range(len(Networks)) :
+                Omega_e0[Networks[n].name] = np.zeros(len(Freq))
+            if orbit_evo == False :
+                for i in range(len(df['Mc'])):
+                    evt = df.iloc[i]
+                    args = [evt['Mc'], evt['q'], (evt['m1']*evt['s1']+evt['m2']*evt['s2'])/(evt['m1']+evt['m2']), evt['z']]
+                    Omg_e0 = GWk_noEcc(args, co_type, 0)
+                    Omega_e0['Total'] += Omg_e0
+                    for N in Networks:
+                        if evt[N] < SNR_thrs[N]:
+                            Omega_e0[N] += Omg_e0
+            Omega_e0.to_csv('Results/Omega_e0/' + cat, index=False, sep='\t')
+
+
+    def Omega_pycbc(self, Networks):
+        fd_table = pd.read_csv('./AuxiliaryFiles/factor_table.dat', index_col=None, sep='\t')
+        for cat in range(len(self.astromodel.catalogs)) :
+            Cat = pd.read_csv('Catalogs/'+self.astromodel.catalogs[cat], delimiter='\t', index_col=None)
+            Omega_e0 = pd.DataFrame({'f':self.Freq-1., 'Total': np.zeros(len(self.Freq))})
+            Ana = self.anadict[self.astromodel.catalogs[cat]]
+            Ana['Total']['N_source'] = len((Cat.z))
+            for N in range(len(Networks)) :
+                Omega_e0[Networks[N].name] = np.zeros(len(self.Freq))
+                Ana[Networks[N].name]['N_source'] =0
+            for evt in range(len(Cat['m1'])):
+                event = Cat.iloc[[evt]]
+                if  self.inclination == 'Rand' :
+                    i = np.random.randint(0, len(fd_table.inc),1, int)
+                    r = fd_table.iloc[i]
+                    event['inc'] = np.arccos(r.inc.values)
+                elif self.inclination == 'Optimal':
+                    event['inc'] = 0.
+                else :
+                    print("inclination error")
+                Omg_e0 = GWk_noEcc_Pycbcwf(evt = event,
+                                           freq = self.Freq,
+                                           approx = self.approx,
+                                           n = evt,
+                                           size_catalogue = len(Cat.z)) * np.power(self.Freq-1.,3.) * K.C / self.astromodel.duration
+                Omega_e0['Total'] += Omg_e0
+                for N in range(len(Networks)):
+                    SNR = 0
+                    for d in Networks[N].compo :
+                        conf = 'f'+str(d.configuration)
+                        SNR+= event[str(d.name)]* fd_table[conf][i[0]]
+                    if float(SNR) < Networks[N].SNR_thrs:
+                        Ana[Networks[N].name]['N_source'] += 1
+                        Omega_e0[Networks[N].name] += Omg_e0
+            Omega_e0.to_csv('Results/Omega_e0/' + self.astromodel.catalogs[cat], index=False, sep='\t')
+            print('Written :  Results/Omega_e0/' , self.astromodel.catalogs[cat])
+
+
