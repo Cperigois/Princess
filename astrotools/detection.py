Add new PICS, Fixed bugs for reading PSD with pycbc, Reshape of detector class, new factor table computation addition of KAGRA and INDIGO config New PSDs for O4 and O5

astrotools/detection.py
@@ -3,6 +3,7 @@ import numpy as np
 import pycbc.psd
 import pandas as pd
 import json
+import math
 import pickle
 from astropy.cosmology import Planck15
 from stochastic import basic_functions as BF
@@ -14,7 +15,7 @@ def initialization():
         detector = Detector(name=params['detector_list'][det]['name'],
                             configuration=params['detector_list'][det]['configuration'],
                             origin=params['detector_list'][det]['origin'],
-                            psd_file=params['detector_list'][det]['psd_file'],
+                            reference=params['detector_list'][det]['reference'],
                             type= params['detector_list'][det]['type'])
         detector.save()
     for net in params['network_list'].keys():
@@ -25,54 +26,93 @@ def initialization():
                           SNR_thrs= params['network_list'][net]['SNR_thrs'])
         network.save()
 
+
 class Detector:
 
-    def __init__(self, name:str, configuration:str =None, origin:str = 'Pycbc', psd_file:str = None, type:str = None):
+    def __init__(self,
+                 name: str,
+                 configuration: str = None,
+                 origin: str = 'Princess',
+                 reference: str = None,
+                 type: str = None,
+                 psd_file: str =None):
         """
         Instance of a detector.
+
         Parameters
         ----------
-        :param name (str): Name of the detector, will be used in labeling further data as the signal-to-noise-ratio.
-        :param configuration (str): Location, orientation and arm opening of the detector. Should only be chosen among
-            preset configurations 'H', 'L', 'V', 'ET' standing for LIGO-Hanford, LIGO-Livingston, Virgo, and
-            Einstein Telescope (i.e. Triangle, anywhereon the globe) respectively
-        :param origin (str): Place where is set the psd of the detector. Available options are 'Pycbc', 'Princess', or 'User'
-        :param psd_file (str): Refers to the file containing the PSD. If origin is 'Pycbc', it refers to the name of the PSD.
-            It origin is Princess, refers to the name of the file in AuxiliaryFiles/PSDs. If origin is 'User', refers to
-            the path to the sentitivity.
-        :param freq (np.1Darray): Frequency array for the detector. Please care to have it coherent with the frequency
-            range of the detector.
+        name (str): Name of the detector, used for labeling further data such as the signal-to-noise ratio.
+        configuration (str): Location, orientation, and arm opening of the detector. Options: 'H', 'L', 'V', 'ET'.
+        origin (str): Source of the PSD ('Pycbc', 'Princess', or 'User').
+        psd_file (str): File or identifier for a customized PSD. Varies based on origin.
+        reference (str): Reference to existing detectors.
+        type (str): Optional, additional type information.
         """
         self.name = name
-        if (not os.path.exists('Run/' + params['name_of_project_folder'] + '/' + self.name + '_DET.pickle')):
-            # Set class variables
-            self.name = name
-            self.psd_file = psd_file
-            self.origin = origin
-            self.configuration = configuration
-            print(self.name)
-            self.type = type
-            self.make_frequency()
-            if self.freq is None:
-                print('Unable to find th frequency range of the detector... \n Please add in your detector definition freq = [np.array] and recompile your detector')
-        else :
+        # Check if the detector needs to be reloaded or created
+        project_folder = os.path.join('Run', params['name_of_project_folder'])
+        detector_file = os.path.join(project_folder, f'{self.name}_DET.pickle')
+        print(detector_file)
+
+        if not os.path.exists(detector_file) or params['overwrite']['detectors']:
+            self.initialize_detector(_configuration= configuration,
+                                     _reference= reference,
+                                     _origin= origin,
+                                     _type= type,
+                                     _psd_file= psd_file)
+            self.save()
+            print(f'Detector {self.name} successfully saved.')
+        else:
             self.load()
+            print(f'Detector {self.name} successfully loaded.')
 
+
+    def get_psd_file(self):
+        "Get the correct file where to find the Psd"
+        print(self.reference)
+        if self.origin == 'Princess' :
+            self.psd_file = params['detector_params']['psd_attributes'][self.reference]['psd_name']
+        else :
+            print("No customized psd available yet")
+
+    def initialize_detector(self, _configuration, _reference, _origin, _psd_file, _type):
+        """Initialize the detector, setting up necessary parameters."""
+        self.configuration = _configuration
+        self.origin = _origin
+        self.reference = _reference
+        if _psd_file == None:
+            self.get_psd_file()
+        else:
+            self.psd_file = _psd_file
+        self.type = _type
+
+        self.make_frequency()
         if self.freq is None:
-            print('Unable to find th frequency range of the detector... \n Please add in your detector definition freq = [np.array] and recompile your detector')
+            self.handle_missing_frequency()
 
+    def handle_missing_frequency(self):
+        """Handle the case where frequency information is missing."""
+        print(
+            f"Unable to find the frequency range for detector '{self.name}'.\n"
+            "Please define 'freq = [np.array]' in your detector definition and recompile."
+        )
 
     def make_frequency(self):
-        frequency_min = max(params['detector_params']['types'][self.type]['freq']['min'], params['detector_params']['psd_attributes'][self.psd_file]['min_freq'])
-        frequency_max = min(params['detector_params']['types'][self.type]['freq']['max'], params['detector_params']['psd_attributes'][self.psd_file]['max_freq'])
-        n = params['frequency_size']
+        frequency_min = max(params['detector_params']['types'][self.type]['freq']['min'], params['detector_params']['psd_attributes'][self.reference]['min_freq'])
+        frequency_max = min(params['detector_params']['types'][self.type]['freq']['max'], params['detector_params']['psd_attributes'][self.reference]['max_freq'])
+
+        n = max(params['frequency_size'], params['detector_params']['types'][self.type]['freq']['min_fsize'])
+
         scale = params['detector_params']['types'][self.type]['freq']['scale']
+
         if scale =='log':
             self.freq = np.logspace(numpy.log10(frequency_min), numpy.log10(frequency_max), num = n)
+
         elif scale =='lin':
             self.freq = np.linspace(frequency_min, frequency_max, n)
             self.deltaf = self.freq[1]-self.freq[0]
             self.make_psd()
+
         else :
             print ("Error in frequency array creation, please check the type, and eventually avanced param file.")
 
@@ -87,16 +127,22 @@ class Detector:
                                     low_freq_cutoff=float(self.freq[0]))
             self.psd = self.psd[1:len(self.freq)+1]
         elif self.origin == 'Princess' :
-            path = 'AuxiliaryFiles/PSDs/'+self.psd_file+'_psd.dat'
+            path = 'AuxiliaryFiles/PSDs/'+self.psd_file+'.dat'
             df = pd.read_csv(path, index_col = None, sep = '\t')
-            newpath = 'Run/temp/'+self.psd_file+'_psd.dat'
+            newpath = 'Run/temp/'+self.psd_file+'.dat'
             df.to_csv(newpath, index = False, sep = '\t', header = False)
-            print( (len(self.freq)+1) * max(int(self.freq[1] - self.freq[0]),1) )
+            print(self.freq)
+            """ Notes for the use of Pycbc.psd.read.from_numpy_array.
+            - carefully keep the lenght +1
+            - math.ceil(self.freq[0]) rounds freq[0] to the integer above, insuring that the interpolation stays in the right range
+            - delta_f needs to be 1 otherwise pycbc.psd.read.from_numpy_arrays crashes
+            This way to do psd definitely needs to be changed : it refer to pycbc classes, and are not well adapted to Princess. 
+            This issue needs to be address in the V2 of Princess.  
+            """
             self.psd = pycbc.psd.read.from_numpy_arrays(np.array(df['f']), np.array(df['psd[1/Hz]']),
                                                         length=len(self.freq)+1,
-                                                        delta_f=self.freq[1] - self.freq[0],
-                                                        #delta_f=max(int(self.freq[1] - self.freq[0]),1),
-                                                        low_freq_cutoff=self.freq[0])
+                                                        delta_f=1.,
+                                                        low_freq_cutoff=np.ceil(self.freq[0]))
 
             self.psd = self.psd[1:]
         elif self.origin == 'User' :
@@ -141,7 +187,6 @@ class Detector:
         m1 = mtot * q * (1. + z) / (1 + q)
         m2 = m1 / q
         flim = BF.fcut_f(m1=m1, m2=m2, xsi=0, zm=z)
-        print(flim)
         if flim > self.freq[0]+0.15:
             psd = self.Make_psd()
             hp, hc = pycbc.waveform.get_fd_waveform(approximant=waveform_approx, mass1=m1 * (1. + z), mass2 = m2 * (1. + z),
@@ -154,10 +199,11 @@ class Detector:
         else :
             snr_l = 0
         return snr_l
+
     def load(self):
         """try load self.name.txt"""
         path = './Run/' + params['name_of_project_folder'] + '/'
-        file = open(path + self.name + '_DET.pickle', 'rb')
+        file = open(path + name + '_DET.pickle', 'rb')
         data_pickle = file.read()
         file.close()
         self.__dict__ = pickle.loads(data_pickle)
@@ -194,13 +240,18 @@ class Network:
             self.compo = compo
             self.pic_file = pic_file
             keys = list(compo.keys())
-            self.freq = Detector(name = keys[0]).freq
             self.efficiency = efficiency
             self.SNR_thrs = SNR_thrs
             self.duration = duration
-            self.type = Detector(name = keys[0]).type
+            self.get_detectors_attributes(keys[0])
         else :
             self.load()
+
+    def get_detectors_attributes(self, name_detector):
+        with open(f"Run/{params['name_of_project_folder']}/{name_detector}_DET.pickle", 'rb') as file:
+            detector_instance = pickle.load(file)
+        self.freq = detector_instance['freq']
+        self.type = detector_instance['type']
 
     def reshape_pic(self, delimiter:str='\t', Header:bool=False, index:bool=False):
         """

stochastic/background.py
@@ -170,7 +170,7 @@ class Princess:
                             Omega_e0[N] += Omg_e0
             Omega_e0.to_csv('Results/Omega_e0/' + cat, index=False, sep='\t')
 
-    def Omega_pycbc(self):
+    def Omega_pycbc_old(self):
         fd_table = pd.read_csv('./AuxiliaryFiles/factor_table.dat', index_col=None, sep='\t')
         Freq_GB = np.linspace(1,2500,2500)
         waveform = params['detector_params']['types']['3G']['waveform']
@@ -210,10 +210,103 @@ class Princess:
                         elif self.inclination == 'Optimal':
                             fd = 1
                         SNR+= event[d]* fd
-                    if event[network.name].any() < network.SNR_thrs:
-                        Ana[network.name]['N_source'] += 1
-                        Omega_e0[network.name] += Omg_e0
+                    # Loop through each threshold in the list of SNR thresholds
+                    for snr_thr in network.SNR_thrs:
+                        if event[network.name] < snr_thr:
+                            col_name = network.name+'_thrs_'+str(snr_thr)
+                            # Initialize the dictionary for this threshold if not already present
+                            if snr_thr not in Ana[network.name]:
+                                Ana[col_name] = {'N_source': 0}
+
+                            # Increment the source count for this threshold
+                            Ana[col_name]['N_source'] += 1
+
+                            # Ensure the Omega_e0 dictionary has an entry for this threshold
+                            if col_name not in Omega_e0.keys:
+                                Omega_e0[col_name] = np.zeros(len(Freq_GB))
+
+                            # Add to the Omega_e0 value for this threshold
+                            Omega_e0[col_name] += Omg_e0
             Omega_e0.to_csv('Run/' + params['name_of_project_folder'] + "/Results/Omega/" + self.astromodel.catalogs[cat], index=False, sep='\t')
             print('Written : Run/' + params['name_of_project_folder'] + "/Results/Omega/" , self.astromodel.catalogs[cat])
+
+    def Omega_pycbc(self):
+        # Load factor table
+        fd_table = pd.read_csv('./AuxiliaryFiles/factor_table.dat', sep='\t')
+        Freq_GB = np.linspace(1, 2500, 2500)
+        waveform = params['detector_params']['types']['3G']['waveform']
+
+        # Loop through all catalogs
+        for cat_path in self.astromodel.catalogs:
+            # Load catalog data
+            catalog_path = f'./Run/{params["name_of_project_folder"]}/Astro_Models/Catalogs/{cat_path}'
+            Cat = pd.read_csv(catalog_path, delimiter='\t')
+
+            # Initialize Omega and analysis dictionaries
+            Omega_e0 = pd.DataFrame({'f': Freq_GB - 1., 'Total': np.zeros(len(Freq_GB))})
+            Ana = self.anadict[cat_path]
+            Ana['Total']['N_source'] = len(Cat.z)
+
+            # Initialize network-specific data
+            for network in self.net_list_GB:
+                Omega_e0[network.name] = np.zeros(len(Freq_GB))
+                Ana[network.name]['N_source'] = 0
+
+            # Process each event in the catalog
+            for _, event in Cat.iterrows():
+                # Handle inclination
+                if self.inclination == 'Rand':
+                    i = np.random.randint(0, len(fd_table))
+                    event['inc'] = np.arccos(fd_table.iloc[i].inc)
+                elif self.inclination == 'Optimal':
+                    event['inc'] = 0.0
+                else:
+                    raise ValueError("Invalid inclination value. Must be 'Rand' or 'Optimal'.")
+
+                # Calculate Omega for the event
+                Omg_e0 = (
+                        GWk_no_ecc_pycbcwf(
+                            evt=event,
+                            freq=Freq_GB,
+                            approx=waveform,
+                            n=event.name,  # Row index
+                            size_catalogue=len(Cat.z)
+                        )
+                        * np.power(Freq_GB - 1., 3.)
+                        * K.C
+                        / self.astromodel.duration
+                )
+                Omega_e0['Total'] += Omg_e0
+
+                # Evaluate for each network
+                for network in self.net_list_GB:
+                    if network.name not in event.keys():
+                        for d in network.compo.keys():
+                            detector = DET.Detector(name=d)
+                            conf = f'f{detector.configuration}'
+                            fd = fd_table[conf][i] if self.inclination == 'Rand' else 1
+                            SNR += event[d] * fd
+                    else : SNR = event[network.name]
+
+                    # Handle thresholds for the network
+                    for snr_thr in network.SNR_thrs:
+                        col_name = f"{network.name}_thrs_{snr_thr}"
+
+                        if SNR < snr_thr:
+                            # Initialize analysis dictionary for threshold if not present
+                            Ana.setdefault(col_name, {'N_source': 0})
+                            Ana[col_name]['N_source'] += 1
+
+                            # Ensure Omega_e0 column exists for this threshold
+                            if col_name not in Omega_e0:
+                                Omega_e0[col_name] = np.zeros(len(Freq_GB))
+
+                            Omega_e0[col_name] += Omg_e0
+
+            # Save Omega results to file
+            result_path = f'Run/{params["name_of_project_folder"]}/Results/Omega/{cat_path}'
+            Omega_e0.to_csv(result_path, index=False, sep='\t')
+            print(f'Written: {result_path}')
+
 
 

stochastic/basic_functions.py
@@ -1,10 +1,12 @@
 import math
 import numpy as np
 import pandas as pd
+import csv as csv
 from scipy.integrate import quad
 from scipy.interpolate import InterpolatedUnivariateSpline
 from astropy.cosmology import Planck15
-
+import os
+import pickle
 
 
 def m1_m2_to_mc_q(m1, m2):
@@ -183,3 +185,181 @@ def build_interp():
 	table = pd.DataFrame({'z': z, 'dl': dl})
 	table.to_csv('./AuxiliaryFiles/dl_z_table_Planck_15.txt', index = None, sep = '\t')
 
+def reshape_psd(file_input, name_output):
+	input_file = './AuxiliaryFiles/PSDs/'+file_input
+	with open(input_file, 'r') as file:
+		sample = file.read(200)  # Lire un échantillon du fichier
+		dialect = csv.Sniffer().sniff(sample)
+		separator = dialect.delimiter  # Le séparateur est ici
+		print(f"Guessed separator : '{separator}'")
+	df = pd.read_csv('./AuxiliaryFiles/PSDs/'+file_input, sep=separator, index_col = None, header = None)
+
+	print(df.describe())
+
+	output = pd.DataFrame({'f': df[0]})
+	output['psd[1/Hz]'] = df[1]**2
+
+	output_file = f'./AuxiliaryFiles/PSDs/{name_output}_psd.dat'
+	output.to_csv(output_file, sep = '\t', index = None)
+	print(f"File reshaped and saved : {output_file}")
+	print(output.describe())
+
+def load_detector(name_detector: str, project_folder: str = "/Run") -> object:
+    """
+    Load a Detector instance from a pickle file.
+
+    Parameters
+    ----------
+    name_detector : str
+        The name of the detector.
+    project_folder : str, optional
+        The base directory where the detector pickle files are stored, by default 'Run'.
+
+    Returns
+    -------
+    object
+        The loaded Detector instance, or None if loading fails.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the specified detector file does not exist.
+    Exception
+        For any other issues during the loading process.
+    """
+
+    file_path = os.path.join(project_folder, f"{name_detector}_DET.pickle")
+    try:
+        with open(file_path, "rb") as file:
+            detector_instance = pickle.load(file)
+            print(f"Detector '{name_detector}' successfully loaded from {file_path}.")
+            return detector_instance
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred while loading the detector: {e}")
+        return None
+
+
+Matrice_peach = [' ***   GW COMPUTATION   ***         ',
+				 u'           \u25A0 \u25A0\u25A0 \u25A0             2%    ',
+				 u'          \u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0            4%    ',
+				 u'          \u25A0 \u25A0  \u25A0 \u25A0            6%    ',
+				 u'         \u25A0        \u25A0           8%    ',
+				 u'         \u25A0        \u25A0           10%   ',
+				 u'          \u25A0\u25A0\u25A0\u25A0 \u25A0\u25A0\u25A0            12%   ',
+				 u'         \u25A0    \u25A0  \u25A0\u25A0           14%   ',
+				 u'        \u25A0          \u25A0          16%   ',
+				 u'      \u25A0\u25A0            \u25A0         18%   ',
+				 u' \u25A0   \u25A0              \u25A0\u25A0        20%   ',
+				 u'  \u25A0\u25A0\u25A0                 \u25A0       22%   ',
+				 u' \u25A0                    \u25A0       24%   ',
+				 u'  \u25A0                  \u25A0        26%   ',
+				 u' \u25A0         \u25A0\u25A0     \u25A0\u25A0  \u25A0       28%   ',
+				 u'  \u25A0       \u25A0  \u25A0   \u25A0 \u25A0  \u25A0       30%   ',
+				 u'   \u25A0      \u25A0   \u25A0\u25A0\u25A0 \u25A0\u25A0 \u25A0        32%   ',
+				 u'    \u25A0\u25A0 \u25A0\u25A0 \u25A0  \u25A0   \u25A0 \u25A0\u25A0         34%   ',
+				 u'    \u25A0 \u25A0 \u25A0 \u25A0  \u25A0   \u25A0 \u25A0\u25A0         36%   ',
+				 u'   \u25A0  \u25A0 \u25A0  \u25A0 \u25A0   \u25A0 \u25A0\u25A0\u25A0        38%   ',
+				 u'\u25A0\u25A0\u25A0\u25A0   \u25A0 \u25A0 \u25A0       \u25A0 \u25A0        40%   ',
+				 u' \u25A0    \u25A0   \u25A0   \u25A0\u25A0  \u25A0 \u25A0         42%   ',
+				 u' \u25A0     \u25A0 \u25A0 \u25A0     \u25A0\u25A0           44%   ',
+				 u'  \u25A0\u25A0  \u25A0\u25A0\u25A0\u25A0\u25A0 \u25A0\u25A0\u25A0\u25A0\u25A0  \u25A0          46%   ',
+				 u' \u25A0   \u25A0     \u25A0     \u25A0  \u25A0\u25A0        48%   ',
+				 u'  \u25A0 \u25A0       \u25A0     \u25A0  \u25A0        50%   ',
+				 u'  \u25A0 \u25A0       \u25A0     \u25A0  \u25A0        52%   ',
+				 u'  \u25A0 \u25A0      \u25A0      \u25A0  \u25A0        54%   ',
+				 u'  \u25A0  \u25A0    \u25A0\u25A0      \u25A0 \u25A0         56%   ',
+				 u'   \u25A0  \u25A0\u25A0\u25A0\u25A0  \u25A0    \u25A0  \u25A0         58%   ',
+				 u'   \u25A0\u25A0    \u25A0   \u25A0   \u25A0  \u25A0         60%   ',
+				 u'\u25A0\u25A0\u25A0\u25A0    \u25A0\u25A0    \u25A0  \u25A0 \u25A0          62%   ',
+				 u' \u25A0     \u25A0  \u25A0    \u25A0\u25A0  \u25A0          64%   ',
+				 u'  \u25A0   \u25A0    \u25A0     \u25A0 \u25A0          66%   ',
+				 u'   \u25A0 \u25A0      \u25A0     \u25A0 \u25A0         68%   ',
+				 u'    \u25A0        \u25A0     \u25A0 \u25A0        70%   ',
+				 u'              \u25A0   \u25A0  \u25A0        72%   ',
+				 u'   \u25A0  \u25A0        \u25A0\u25A0\u25A0    \u25A0       74%   ',
+				 u'  \u25A0  \u25A0    \u25A0       \u25A0   \u25A0       76%   ',
+				 u' \u25A0   \u25A0   \u25A0         \u25A0   \u25A0      78%   ',
+				 u' \u25A0   \u25A0   \u25A0         \u25A0   \u25A0      80%   ',
+				 u' \u25A0  \u25A0   \u25A0           \u25A0  \u25A0      82%   ',
+				 u'\u25A0   \u25A0   \u25A0           \u25A0  \u25A0      84%   ',
+				 u'\u25A0   \u25A0   \u25A0           \u25A0   \u25A0     86%   ',
+				 u'\u25A0   \u25A0   \u25A0           \u25A0   \u25A0     88%   ',
+				 u' \u25A0  \u25A0   \u25A0           \u25A0   \u25A0     90%   ',
+				 u'  \u25A0\u25A0\u25A0   \u25A0           \u25A0 \u25A0\u25A0      92%   ',
+				 u'     \u25A0\u25A0\u25A0\u25A0\u25A0\u25A0        \u25A0\u25A0\u25A0        94%   ',
+				 u'           \u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0           96%   ',
+				 u'                              98%   ',
+				 u'   ***   GW COMPUTED   ***    100%  ', ]
+
+Matrice_Leia = [u'    *** START ***     0%   ',
+				u'        \u25A0\u25A0\u25A0\u25A0\u25A0         4%   ',
+				u'      \u25A0\u25A0   \u25A0 \u25A0\u25A0       8%   ',
+				u'     \u25A0    \u25A0    \u25A0      12%  ',
+				u'    \u25A0     \u25A0     \u25A0     16%  ',
+				u'  \u25A0\u25A0\u25A0     \u25A0     \u25A0\u25A0\u25A0   20%  ',
+				u' \u25A0  \u25A0    \u25A0\u25A0\u25A0    \u25A0  \u25A0  24%  ',
+				u' \u25A0  \u25A0  \u25A0\u25A0   \u25A0\u25A0  \u25A0  \u25A0  28%  ',
+				u' \u25A0\u25A0 \u25A0\u25A0\u25A0       \u25A0\u25A0\u25A0 \u25A0\u25A0  32%  ',
+				u' \u25A0\u25A0 \u25A0           \u25A0 \u25A0\u25A0  36%  ',
+				u' \u25A0  \u25A0   \u25A0    \u25A0  \u25A0  \u25A0  40%  ',
+				u' \u25A0  \u25A0  \u25A0\u25A0   \u25A0\u25A0  \u25A0  \u25A0  44%  ',
+				u'  \u25A0\u25A0\u25A0           \u25A0\u25A0\u25A0   48%  ',
+				u'     \u25A0    \u25A0    \u25A0      52%  ',
+				u'      \u25A0\u25A0     \u25A0\u25A0       56%  ',
+				u'\u25A0\u25A0   \u25A0  \u25A0\u25A0\u25A0\u25A0\u25A0  \u25A0      60%  ',
+				u'\u25A0 \u25A0\u25A0\u25A0           \u25A0     64%  ',
+				u' \u25A0  \u25A0            \u25A0    68%  ',
+				u'  \u25A0  \u25A0\u25A0       \u25A0   \u25A0   72%  ',
+				u'  \u25A0  \u25A0\u25A0       \u25A0\u25A0  \u25A0   76%  ',
+				u'   \u25A0\u25A0 \u25A0       \u25A0 \u25A0\u25A0    80%  ',
+				u'      \u25A0       \u25A0       84%  ',
+				u'     \u25A0         \u25A0      88%  ',
+				u'     \u25A0         \u25A0      92%  ',
+				u'     \u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0\u25A0      96%  ',
+				u'  *** COMPLETED ***   100% ']
+
+
+# def bar_error() :
+def bar_peach(n, ntot):
+	"""
+    Affiche progressivement la progression en utilisant Matrice_peach.
+    Parameters:
+        n (int): Valeur actuelle de la progression.
+        ntot (int): Valeur totale à atteindre.
+    """
+	try:
+		# Calcul du nombre de paliers (assurez-vous que ntot est suffisant pour 50 étapes)
+		steps = max(ntot // 50, 1)
+
+		# Affichage uniquement à chaque palier
+		if n % steps == 0:
+			# Calcul de l'index dans la matrice
+			index = min(n // steps, len(Matrice_peach) - 1)
+			# Affiche la ligne correspondante
+			print(Matrice_peach[index] + f" {n}/{ntot}")
+	except (ZeroDivisionError, IndexError) as e:
+		print(f"Erreur dans l'affichage de la barre de progression : {e}")
+
+
+def bar_leia(n, ntot):
+	"""
+    Affiche progressivement la progression en utilisant Matrice_peach.
+    Parameters:
+        n (int): Valeur actuelle de la progression.
+        ntot (int): Valeur totale à atteindre.
+    """
+	try:
+		# Calcul du nombre de paliers (assurez-vous que ntot est suffisant pour 50 étapes)
+		steps = max(ntot // 50, 1)
+
+		# Affichage uniquement à chaque palier
+		if n % steps == 0:
+			# Calcul de l'index dans la matrice
+			index = min(n // steps, len(Matrice_leia) - 1)
+			# Affiche la ligne correspondante
+			print(Matrice_peach[index] + f" {n}/{ntot}")
+	except (ZeroDivisionError, IndexError) as e:
+		print(f"Erreur dans l'affichage de la barre de progression : {e}")